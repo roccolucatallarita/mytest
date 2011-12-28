27474bc65f9026d6a977c9d3e225e7fe3f81aee7
Your Name <r.l.t@hotmail.it>
2011-12-28 07:08:16 +0100

    fine pagine statiche

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,18 @@
 module ApplicationHelper
+ 
+  def root_url
+    'http://localhost:3000/'
+  end
+
+  def root_path 
+    '/'
+  end
+  def logo
+    image_tag('rails.png');
+  end
+  def help_path
+    '/help'
+  end
   def title
     base_title = "Ruby on Rails Tutorial Sample App"
     if @title.nil?
--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Mytest::Application.routes.draw do
   match '/help',    :to => 'pages#help'
   match '/signup',  :to => 'users#new'  
   root :to => 'pages#home'
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
